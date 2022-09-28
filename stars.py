db064f97973469f6d57cdb801a9a51d90d83e11a
aykae <raiaboudi@yahoo.com>
2022-09-27 23:41:11 -0400

    floating point comparison issues

--- stars.py
+++ stars.py
@@ -3,8 +3,8 @@ import hub75, time, random
 WIDTH = 64
 HEIGHT = 64
 MAX_BRIGHTNESS = 255
-MAX_STARS = 100
-SPEED = 0.5
+MAX_STARS = 1
+SPEED = 1
 
 matrix = hub75.Hub75(WIDTH, HEIGHT)
 matrix.start()
@@ -25,18 +25,20 @@ def loop():
     #SELECT RANDOM STAR 
     nextStar = (random.randint(0, WIDTH-1), random.randint(0, HEIGHT-1))
     currTime = time.time()
-    if nextStar not in stars.keys() and starCount < MAX_STARS and currTime - prevStarTime >= delay:
-        stars[nextStar] = [1, 0] #THIS MAY
-        starCount += 1
-
+    if nextStar not in stars.keys() and starCount < MAX_STARS and (currTime - prevStarTime) >= delay:
         prevStarTime = time.time()
-        delay = 0.2 * random.random()
+        delay = 0.1 * random.random()
+
+        stars[nextStar] = [1, 0, 0] #
+        starCount += 1
 
     #INCREMENT STAR BRIGHTNESS ACCORDING TO DIRECTION
     for star in stars.keys():
         if stars[star][0] == 1:
             if stars[star][1] >= MAX_BRIGHTNESS:
-                stars[star][0] = -1
+                stars[star][0] = 2
+                dimDelay = 2.0 * random.random()
+                stars[star][2] = time.time() + dimDelay #assign dim delay
             else:
                 stars[star][1] = min(stars[star][1] + SPEED, MAX_BRIGHTNESS) #brighten star
         elif stars[star][0] == -1:
@@ -45,6 +47,10 @@ def loop():
                 starCount -= 1
             else:
                 stars[star][1] = max(stars[star][1] - SPEED, 0) #dim star
+        elif stars[star][0] == 2:
+            if int(time.time()) > int(stars[star][2]):
+                stars[star][2] = 0
+                stars[star][0] = -1
             
     updateMatrix()
 
@@ -58,4 +64,3 @@ def updateMatrix():
 setup()
 while True:
     loop()
-    #time.sleep(0.01)
