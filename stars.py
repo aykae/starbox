4743c1070ba07776c6a8ea35ca102b50acd8e3f9
aykae <raiaboudi@yahoo.com>
2022-10-13 20:36:45 -0400

    flicker works, but doesn't look good

--- stars.py
+++ stars.py
@@ -3,6 +3,8 @@ import hub75, time, random
 WIDTH = 32
 HEIGHT = 32
 MAX_BRIGHTNESS = 255
+MIN_FLICKER = 20 
+MAX_FLICKER = 25
 MAX_STARS = 500
 SPEED = 1
 
@@ -20,43 +22,81 @@ def setup():
 
 
 def loop():
-    global starCount, stars, prevStarTime, delay
+    global starCount, stars, prevStarTime, prevStarCount, delay
 
     #SELECT RANDOM STAR
     if starCount < MAX_STARS:
-        nextStar = (random.randint(0, WIDTH-1), random.randint(0, HEIGHT-1))
-        if nextStar not in stars.keys() and (time.time_ns() - prevStarTime) >= delay:
-            prevStarTime = time.time_ns()
-            delay = (10**9) * random.randint(0, 1) # 1-2 seconds
+        prevStarCount = starCount
+        if (time.time_ns() - prevStarTime) >= delay:
+            while starCount == prevStarCount: #while new star not yet added
+                nextStar = (random.randint(0, WIDTH-1), random.randint(0, HEIGHT-1))
+                hasAdjacent = checkForAdjacent(nextStar)
+                if nextStar not in stars.keys() and (time.time_ns() - prevStarTime) >= delay and not hasAdjacent:
+                    prevStarTime = time.time_ns()
+                    delay = (10**9) * random.randint(0, 1)
 
-            # [star_state, dim_level, dim_delay]
-                # star_state: 0 -> inactive, 1 -> brightening, 2 -> peaking, -1 -> dimming
-                # dim_level: 0 -> 255 (white value)
-                # dim_delay: float of a time at which to begin the dimming process
-            stars[nextStar] = [1, 0, 0] 
-            starCount += 1
+                    # [starState, dimLevel, dimDelay, flickerDir]
+                        # starState: 0 -> inactive, 1 -> brightening, 2 -> peaking, -1 -> dimming
+                        # dimLevel: 0 -> 255 (white value)
+                        # dimDelay: float of a time at which to begin the dimming process
+                        # flickerDir: direction of flicker, either -1 or 1
+                    stars[nextStar] = [1, 0, 0, -1] 
+                    starCount += 1
 
     #INCREMENT STAR BRIGHTNESS ACCORDING TO DIRECTION
     for star in stars.keys():
         if stars[star][0] == 1: #brightenining
-            if stars[star][1] >= MAX_BRIGHTNESS:
+            if stars[star][1] == MAX_BRIGHTNESS:
                 stars[star][0] = 2
-                dimDelay = (10**9) * random.randint(3, 10)
+                dimDelay = (10**9) * random.randint(3, 6)
                 stars[star][2] = time.time_ns() + dimDelay #assign dim delay
             else:
                 stars[star][1] = min(stars[star][1] + SPEED, MAX_BRIGHTNESS) #brighten star
         elif stars[star][0] == -1: #dimming
-            if stars[star][1] <= 0:
+            if stars[star][1] == 0:
                 stars.pop(star) #remove star from active dict
                 starCount -= 1
             else:
                 stars[star][1] = max(stars[star][1] - SPEED, 0) #dim star
         elif stars[star][0] == 2: #shining (static)
-            if time.time_ns() > stars[star][2]:
+            if time.time_ns() > stars[star][2]: #begin dimming
                 stars[star][2] = 0
                 stars[star][0] = -1
+            else: #flicker
+                #flickerVal = random.randint(0, MAX_FLICKER)
+                flickerVal = random.randint(0, MAX_FLICKER)
+                flickerDir = stars[star][3]
+                if flickerDir == 1:
+                    stars[star][1] = min(MAX_BRIGHTNESS, stars[star][1] + flickerVal)
+                    stars[star][3] = -1
+                if flickerDir == -1:
+                    stars[star][1] = max(0, stars[star][1] - flickerVal)
+                    stars[star][3] = 1
+                
+
+
+
+
             
     updateMatrix()
+
+def checkForAdjacent(nextStar):
+    global stars
+
+    keys = stars.keys()
+    nsX = nextStar[0]
+    nsY = nextStar[1]
+
+    if (nsX + 1, nsY) in keys:
+        return True
+    elif (nsX - 1, nsY) in keys:
+        return True
+    elif (nsX, nsY + 1) in keys:
+        return True
+    elif (nsX, nsY - 1) in keys:
+        return True
+
+    return False
 
 def updateMatrix():
     for star in stars.keys():
