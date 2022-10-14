30585cdcec8164f638f64e2b9d0d1ac77e674ca8
aykae <raiaboudi@yahoo.com>
2022-10-13 20:13:07 -0400

    delay is functional, still needs aesthetic work

--- stars.py
+++ stars.py
@@ -3,7 +3,7 @@ import hub75, time, random
 WIDTH = 32
 HEIGHT = 32
 MAX_BRIGHTNESS = 255
-MAX_STARS = 50
+MAX_STARS = 500
 SPEED = 1
 
 matrix = hub75.Hub75(WIDTH, HEIGHT)
@@ -25,10 +25,9 @@ def loop():
     #SELECT RANDOM STAR
     if starCount < MAX_STARS:
         nextStar = (random.randint(0, WIDTH-1), random.randint(0, HEIGHT-1))
-        currTime = time.time_ns()
-        if nextStar not in stars.keys() and (currTime - prevStarTime) >= delay:
+        if nextStar not in stars.keys() and (time.time_ns() - prevStarTime) >= delay:
             prevStarTime = time.time_ns()
-            delay = random.random()
+            delay = (10**9) * random.randint(0, 1) # 1-2 seconds
 
             # [star_state, dim_level, dim_delay]
                 # star_state: 0 -> inactive, 1 -> brightening, 2 -> peaking, -1 -> dimming
@@ -42,7 +41,7 @@ def loop():
         if stars[star][0] == 1: #brightenining
             if stars[star][1] >= MAX_BRIGHTNESS:
                 stars[star][0] = 2
-                dimDelay = (10**9) * random.randint(1,2) # 1-2 seconds
+                dimDelay = (10**9) * random.randint(3, 10)
                 stars[star][2] = time.time_ns() + dimDelay #assign dim delay
             else:
                 stars[star][1] = min(stars[star][1] + SPEED, MAX_BRIGHTNESS) #brighten star
@@ -53,7 +52,7 @@ def loop():
             else:
                 stars[star][1] = max(stars[star][1] - SPEED, 0) #dim star
         elif stars[star][0] == 2: #shining (static)
-            if time.time() > stars[star][2]:
+            if time.time_ns() > stars[star][2]:
                 stars[star][2] = 0
                 stars[star][0] = -1
             
