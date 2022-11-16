5b678047ba23ee8e9c4b8907342db3aec85d4c4f
aykae <raiaboudi@yahoo.com>
2022-11-16 17:41:05 -0500

    slideshow script added

---
+++ imgdisp/imgdisp_slideshow.py
@@ -0,0 +1,110 @@
+import time, random, math
+import json
+
+################
+#MATRIX VARS
+################
+SIM = False
+
+#DIM OF MATRIX
+WIDTH = 32
+HEIGHT = 32
+BORDER = 2
+
+#
+REFRESH = 30
+DELAY = 500
+
+#
+BG = (0, 0, 0)
+
+SPARKLING = True
+SCOUNT = 20
+scolors = []
+sparkles = {}
+
+#
+filenames = []
+fileIndex = 0
+
+#
+img = []
+frame = 0
+frameCount = 0
+
+if SIM:
+    import pygame
+    from matsim import MatrixSim
+    matrix = MatrixSim(WIDTH, HEIGHT)
+else:
+    import hub75
+    matrix = hub75.Hub75(WIDTH, HEIGHT)
+
+def loadImg(f):
+    global img, scolors
+    filename = f
+    print()
+    
+    with open(filename, "r") as file:
+        #Extract Two Sparkle Colors
+        sc1 = [int(c) for c in file.readline().strip().split(" ")]
+        sc2 = [int(c) for c in file.readline().strip().split(" ")]
+        scolors = [sc1, sc2]
+
+        #Extract Img Data
+        img = []
+        for x in range(WIDTH):
+            img.append([])
+            for _ in range(HEIGHT):
+                color = [int(c) for c in file.readline().strip().split(" ")]
+                img[x].append(color)
+
+def drawImg():
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            color = img[x][y]
+            if color != (0, 0, 0):
+                matrix.set_rgb(x, y, color[0], color[1], color[2])
+
+def drawSparkles(colors):
+    for s in sparkles.keys():
+        matrix.set_rgb(s[0], s[1], BG[0], BG[1], BG[2])
+        sparkles.pop(s)
+
+    for _ in range(SCOUNT):
+        x = random.randint(BORDER, WIDTH-1-BORDER)
+        y = random.randint(BORDER, HEIGHT-1-BORDER)
+
+        c = random.choice(colors)
+
+        if sum(img[x][y]) == 0 and (x,y) not in sparkles.keys():
+            matrix.set_rgb(x, y, c[0], c[1], c[2])
+            sparkles[(x,y)] = c
+
+def setup():
+    global img, filenames, fileIndex
+    matrix.start()
+    filenames = ["img.txt", "img1.txt", "img2.txt", "img3.txt", "img4.txt"]
+
+
+def loop():
+    global ani, frame, frameCount, scolors, filenames, fileIndex
+
+    matrix.clear()
+    loadImg(filenames[fileIndex])
+    fileIndex = (fileIndex + 1) % len(filenames)
+    
+    drawImg()
+    #drawSparkles(scolors)
+    matrix.flip()
+    time.sleep(REFRESH / 1000)
+    time.sleep(DELAY / 1000)
+
+
+##############
+# MAIN LOOP
+##############
+
+setup()
+while True:
+    loop()
