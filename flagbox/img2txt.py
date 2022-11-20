636168abfb6c4aa8bc39dcb1530350fedf9e4b02
aykae <raiaboudi@yahoo.com>
2022-11-20 15:29:28 -0500

    flagbox

---
+++ flagbox/img2txt.py
@@ -0,0 +1,40 @@
+from PIL import Image
+import os
+
+def generateAni(dir, output="ani.txt"):
+    num_frames = len(os.listdir(dir))
+    with open(output, "a") as file:
+        file.write(str(num_frames) + "\n")
+
+    for filename in os.listdir(dir):
+        im = Image.open(dir + "/" + filename)
+        im = im.convert('RGB')
+        size = im.size
+        pix = im.load()
+
+        arr = []
+        for x in range(size[0]):
+            arr.append([])
+            for y in range(size[1]):
+                arr[x].append(pix[x, y])
+
+        with open(dir, "a") as file:
+            for x in range(size[0]):
+                for y in range(size[1]):
+                    color = list(pix[x,y])
+                    file.write(" ".join(str(c) for c in color) + "\n")
+
+def generateImg(filename, output="img.txt"):
+    im = Image.open(filename)
+    im = im.convert('RGB')
+    size = im.size
+    pix = im.load()
+    with open(output, "w") as file:
+        for x in range(size[0]):
+            for y in range(size[1]):
+                color = list(pix[x,y])
+                file.write(" ".join(str(c) for c in color) + "\n")
+
+#MAIN
+
+generateImg("img/final/dallas-gc.bmp")
---
+++ flagbox/imgdisp.py
@@ -0,0 +1,104 @@
+import time, random, math
+import json
+
+################
+#MATRIX VARS
+################
+SIM = False
+
+#DIM OF MATRIX
+WIDTH = 32
+HEIGHT = 32
+BORDER = 2
+
+#
+REFRESH = 30
+
+#
+BG = (0, 0, 0)
+
+SPARKLING = True
+SCOUNT = 20
+scolors = []
+sparkles = {}
+#
+filename = ""
+
+#
+img = []
+frame = 0
+frameCount = 0
+
+if SIM:
+    import pygame
+    from matsim import MatrixSim
+    matrix = MatrixSim(WIDTH, HEIGHT)
+else:
+    import hub75
+    matrix = hub75.Hub75(WIDTH, HEIGHT)
+
+def loadImg(f):
+    global img, scolors
+    filename = f
+
+    with open(filename, "r") as file:
+        #Extract Two Sparkle Colors
+        sc1 = [int(c) for c in file.readline().strip().split(" ")]
+        sc2 = [int(c) for c in file.readline().strip().split(" ")]
+        scolors = [sc1, sc2]
+
+        #Extract Img Data
+        for x in range(WIDTH):
+            img.append([])
+            for _ in range(HEIGHT):
+                color = [int(c) for c in file.readline().strip().split(" ")]
+                img[x].append(color)
+def drawImg():
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            color = img[x][y]
+            if color != (0, 0, 0):
+                matrix.set_rgb(x, y, color[0], color[1], color[2])
+
+def drawSparkles(colors):
+    for s in sparkles.keys():
+        matrix.set_rgb(s[0], s[1], BG[0], BG[1], BG[2])
+        sparkles.pop(s)
+
+    for _ in range(SCOUNT):
+        x = random.randint(BORDER, WIDTH-1-BORDER)
+        y = random.randint(BORDER, HEIGHT-1-BORDER)
+
+        c = random.choice(colors)
+
+        if sum(img[x][y]) == 0 and (x,y) not in sparkles.keys():
+            matrix.set_rgb(x, y, c[0], c[1], c[2])
+            sparkles[(x,y)] = c
+
+def setup():
+    global img, filename
+    matrix.start()
+    loadImg("img.txt")
+    drawImg()
+
+    matrix.flip()
+
+
+def loop():
+    global ani, frame, frameCount, scolors
+
+    matrix.clear()
+    drawImg()
+    #drawSparkles(scolors)
+    matrix.flip()
+
+    time.sleep(REFRESH / 1000)
+
+
+##############
+# MAIN LOOP
+##############
+
+setup()
+while True:
+    loop()
