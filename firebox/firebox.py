c77e55b7aa23dcd468994c4d4ea4b2673e83543d
aykae <raiaboudi@yahoo.com>
2022-11-20 17:40:37 -0500

    firebox started

--- firebox/firebox.py
+++ firebox/firebox.py
@@ -14,7 +14,12 @@ HEIGHT = 32
 REFRESH = 50
 
 #
-ani = []
+fireshape = []
+fire = {}
+
+logs = {}
+
+#
 frame = 0
 frameCount = 0
 
@@ -26,42 +31,85 @@ else:
     import hub75
     matrix = hub75.Hub75(WIDTH, HEIGHT)
 
+def genFireshape():
+    fw = 16
+    fh = 16
+    fxoffset = (WIDTH - fw) // 2
+    fyoffset = (HEIGHT - fh) // 2 + 5
+    for x in range(fxoffset, fxoffset + fw):
+        for y in range(fyoffset, fyoffset + fh):
+            fireshape.append((x,y))
+
+
+def genFire():
+    fcolors = {
+        # "WHITE": (255, 255, 255),
+        "DARK_ORANGE": (230, 54, 19),
+        "LIGHT_ORANGE": (237, 109, 25),
+        "DARK_YELLOW": (248, 191, 34),
+        "RED_ORANGE": (227, 27, 16)
+    }
+
+    for f in fireshape:
+        fire[f] = random.choice(list(fcolors.values()))
+
+def drawFire():
+    global fire
+
+    for k in fire.keys():
+        color = fire[k]
+        matrix.set_rgb(k[0], k[1], color[0], color[1], color[2])
+
+def loadLogs(filename):
+    with open(filename, "r") as file:
+        #Extract img data
+        for x in range(WIDTH):
+            for y in range(HEIGHT):
+                color = [int(c) for c in file.readline().strip().split(" ")]
+
+                #soring black is redundant
+                if sum(color) > 0:
+                    logs[(x,y)] = color
+
+
+def drawLogs():
+    global logs
+
+    for k in logs.keys():
+        color = logs[k]
+        matrix.set_rgb(k[0], k[1], color[0], color[1], color[2])
+
+
 def setup():
     global ani, frame, frameCount
     matrix.start()
 
-    with open("ani.txt", "r") as file:
-        #frameCount = int(file.readline().strip())
-        frameCount = 2
+    loadLogs("logs.txt")
+    drawLogs()
 
-        for _ in range(frameCount):
-        #for _ in range(1):
-            for x in range(WIDTH):
-                ani.append([])
-                for y in range(HEIGHT):
-                    color = [int(c) for c in file.readline().strip().split(" ")]
-                    ani[x].append(color)
+    genFireshape()
+    genFire()
 
-    for x in range(WIDTH):
-        for y in range(HEIGHT):
-            color = ani[x][y]
-            matrix.set_rgb(x, y, color[0], color[1], color[2])
+    # with open("ani.txt", "r") as file:
+    #     for _ in range(frameCount):
+    #         for x in range(WIDTH):
+    #             ani.append([])
+    #             for y in range(HEIGHT):
+    #                 color = [int(c) for c in file.readline().strip().split(" ")]
+    #                 ani[x].append(color)
 
-    frame = (frame + 1) % frameCount
 
 
 def loop():
     global ani, frame, frameCount
+
+    drawFire()
+    drawLogs()
     matrix.flip()
 
-    xOffset = frame * WIDTH
-    #xOffset = 0
-    for x in range(WIDTH):
-        for y in range(HEIGHT):
-            color = ani[xOffset + x][y]
-            matrix.set_rgb(x, y, color[0], color[1], color[2])
+    genFire()
 
-    frame = (frame + 1) % frameCount
+
 
 ##############
 # MAIN LOOP
--- firebox/img2txt.py
+++ firebox/img2txt.py
@@ -1,24 +1,42 @@
 from PIL import Image
 import os
 
-num_frames = len(os.listdir('frames'))
-with open("ani.txt", "a") as file:
-    file.write(str(num_frames) + "\n")
+INPUT = "final/logs.bmp"
+OUTPUT = "img.txt"
 
-for filename in os.listdir('frames'):
-    im = Image.open("frames/" + filename)
+def generateAni(dir, output="flag-txt/ani.txt"):
+    num_frames = len(os.listdir('frames'))
+    with open("ani.txt", "a") as file:
+        file.write(str(num_frames) + "\n")
+
+    for filename in os.listdir('frames'):
+        im = Image.open("frames/" + filename)
+        im = im.convert('RGB')
+        size = im.size
+        pix = im.load()
+
+        arr = []
+        for x in range(size[0]):
+            arr.append([])
+            for y in range(size[1]):
+                arr[x].append(pix[x, y])
+
+        with open("ani.txt", "a") as file:
+            for x in range(size[0]):
+                for y in range(size[1]):
+                    color = list(pix[x,y])
+                    file.write(" ".join(str(c) for c in color) + "\n")
+
+def generateImg(filename, output=OUTPUT):
+    im = Image.open(filename)
     im = im.convert('RGB')
     size = im.size
     pix = im.load()
-
-    arr = []
-    for x in range(size[0]):
-        arr.append([])
-        for y in range(size[1]):
-            arr[x].append(pix[x, y])
-
-    with open("ani.txt", "a") as file:
+    with open(output, "w") as file:
         for x in range(size[0]):
             for y in range(size[1]):
                 color = list(pix[x,y])
                 file.write(" ".join(str(c) for c in color) + "\n")
+
+#MAIN
+generateImg(INPUT)
