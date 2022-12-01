9f0e2582498590afa4364119081269ab5c4ea6ba
aykae <abdrai19@gmail.com>
2022-12-01 14:57:05 -0500

    animation generation done

--- firebox/img2txt.py
+++ firebox/img2txt.py
@@ -1,21 +1,26 @@
 from PIL import Image
 import os
 
-INPUT = "final/fire.bmp"
-OUTPUT = "img.txt"
+INPUT = "final/fire-ani"
+OUTPUT = "data/ani.txt"
 
+BLACK_THRESH = (25, 25, 25)
 
 #CREATES A SINGLE ANIMATION FILES THAT IS LIGHTWEIGHT AND EFFICIENT
 #DON'T STORE NOR REDRAW REDUNDENCIES or BLACK PIXELS
 #MAYBE DON'T EVEN DRAW LOGS (using a darkness threshold check e.g. > 700 (brown))
 
-def generateAni(dir, output="ani.txt"):
-    num_frames = len(os.listdir('frames'))
-    with open("ani.txt", "a") as file:
-        file.write(str(num_frames) + "\n")
+def generateAni(dir, output):
+    #clear output file
+    with open(output, "w") as file:
+        pass
 
-    for filename in os.listdir('frames'):
-        im = Image.open("frames/" + filename)
+    #dicts used to prevent redundant pixel redraws
+    prevdict = {}
+    currdict = {}
+
+    for filename in os.listdir(dir):
+        im = Image.open(dir + "/" + filename)
         im = im.convert('RGB')
         size = im.size
         pix = im.load()
@@ -26,11 +31,22 @@ def generateAni(dir, output="ani.txt"):
             for y in range(size[1]):
                 arr[x].append(pix[x, y])
 
-        with open("ani.txt", "a") as file:
+        with open(output, "a") as file:
             for x in range(size[0]):
                 for y in range(size[1]):
-                    color = list(pix[x,y])
-                    file.write(" ".join(str(c) for c in color) + "\n")
+                    if sum(pix[x,y]) > sum(BLACK_THRESH):
+                        if prevdict.get((x,y)) is None or (prevdict.get((x,y)) and prevdict.get((x,y)) != pix[x,y]):
+                            currdict[(x,y)] = pix[x,y]
+
+                            pstr = str(x) + " " + str(y) + " "
+                            color = list(pix[x,y])
+                            cstr = " ".join(str(c) for c in color)
+                            file.write(pstr + cstr + "\n")
+
+            prevdict = currdict
+            currdict = {}
+            #denote end of frame 
+            file.write("FRAME\n")
 
 def generateImg(filename, output=OUTPUT):
     im = Image.open(filename)
@@ -44,4 +60,5 @@ def generateImg(filename, output=OUTPUT):
                 file.write(" ".join(str(c) for c in color) + "\n")
 
 #MAIN
-generateImg(INPUT)
+#generateImg(INPUT)
+generateAni(INPUT, OUTPUT)
